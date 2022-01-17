38898879cd9a2e66ee5fce376b77cbf9b030cda1
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2022-01-17 13:50:06 +0200

    Initial Commit

---
+++ OptionalPattern/OptionalPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptionalPatternApp.swift
+//  OptionalPattern
+//
+//  Created by ramil on 17.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct OptionalPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
